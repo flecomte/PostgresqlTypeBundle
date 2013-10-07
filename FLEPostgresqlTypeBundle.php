fd8003115f96141aea55dbd6325b3e3065f9bf6f
flecomte <flecomte@welcome-media.fr>
2013-10-07 17:46:30 +0200

    change namespace

--- FLEPostgresqlTypeBundle.php
+++ FLEPostgresqlTypeBundle.php
@@ -19,32 +19,32 @@ class FLEPostgresqlTypeBundle extends Bundle
         $conn = $em->getConnection();
 
         if (!Type::hasType('cidr')) {
-            Type::addType('cidr', 'FLE\PostgresqlTypeBundle\Doctrine\DBAL\Types\Cidr');
+            Type::addType('cidr', 'FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types\Cidr');
             $conn->getDatabasePlatform()->registerDoctrineTypeMapping('CIDR', 'cidr');
         }
 
         if (!Type::hasType('box')) {
-            Type::addType('box', 'FLE\PostgresqlTypeBundle\Doctrine\DBAL\Types\Box');
+            Type::addType('box', 'FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types\Box');
             $conn->getDatabasePlatform()->registerDoctrineTypeMapping('BOX', 'box');
         }
 
         if (!Type::hasType('pg_array')) {
-            Type::addType('pg_array', 'FLE\PostgresqlTypeBundle\Doctrine\DBAL\Types\PgArray');
+            Type::addType('pg_array', 'FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types\PgArray');
             $conn->getDatabasePlatform()->registerDoctrineTypeMapping('PG_ARRAY', 'pg_array');
         }
 
         if (!Type::hasType('time_tz')) {
-            Type::addType('time_tz', 'FLE\PostgresqlTypeBundle\Doctrine\DBAL\Types\TimeTz');
+            Type::addType('time_tz', 'FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types\TimeTz');
             $conn->getDatabasePlatform()->registerDoctrineTypeMapping('TIME_TZ', 'time_tz');
         }
 
-        Type::overrideType('datetime', 'FLE\PostgresqlTypeBundle\Doctrine\DBAL\Types\DateTime');
+        Type::overrideType('datetime', 'FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types\DateTime');
         $conn->getDatabasePlatform()->registerDoctrineTypeMapping('DATETIME', 'datetime');
 
-        Type::overrideType('datetimetz', 'FLE\PostgresqlTypeBundle\Doctrine\DBAL\Types\DateTimeTz');
+        Type::overrideType('datetimetz', 'FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types\DateTimeTz');
         $conn->getDatabasePlatform()->registerDoctrineTypeMapping('DATETIMETZ', 'datetimetz');
 
-        Type::overrideType('time', 'FLE\PostgresqlTypeBundle\Doctrine\DBAL\Types\Time');
+        Type::overrideType('time', 'FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types\Time');
         $conn->getDatabasePlatform()->registerDoctrineTypeMapping('TIME', 'time');
     }
 }
