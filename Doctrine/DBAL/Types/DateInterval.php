9a2da1c5a1113d0e5e36e0ce01eeff5aada3c099
flecomte <flecomte@welcome-media.fr>
2013-12-16 17:26:43 +0100

    Add DateInterval Type

---
+++ Doctrine/DBAL/Types/DateInterval.php
@@ -0,0 +1,92 @@
+<?php
+namespace FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+
+class DateInterval extends Type
+{
+    const INTERVAL = 'interval';
+
+    public function getName()
+    {
+        return self::INTERVAL;
+    }
+
+    public function getSQLDeclaration (array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getDoctrineTypeMapping('INTERVAL');
+    }
+
+    public function convertToDatabaseValue ($interval, AbstractPlatform $platform)
+    {
+        if ($interval === null) {
+            return null;
+        }
+
+        $sql = "
+            $interval->y year +
+            $interval->m month +
+            $interval->d day +
+            $interval->h hour +
+            $interval->i minute +
+            $interval->s second
+        ";
+
+        return $sql;
+    }
+
+    public function convertToPHPValue ($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        preg_match('`(?:(?P<year>[0-9]+) (?:year|years))? ?(?:(?P<month>[0-9]+) (?:month|mons|mon))? ?(?:(?P<day>[0-9]+) (?:day|days))? ?(?:(?P<hour>[0-9]{2}):(?P<minute>[0-9]{2}):(?P<second>[0-9]{2}))`', $value, $matches);
+
+        if ($matches['year']) {
+            $y = $matches['year'].'Y';
+        } else {
+            $y = null;
+        }
+        if ($matches['month']) {
+            $m = $matches['month'].'M';
+        } else {
+            $m = null;
+        }
+        if ($matches['day']) {
+            $d = $matches['day'].'D';
+        } else {
+            $d = null;
+        }
+
+        if ($y !== null || $m !== null || $d !== null) {
+            $p = $y.$m.$d;
+        }
+
+
+        if ($matches['hour']) {
+            $h = $matches['hour'].'H';
+        } else {
+            $h = null;
+        }
+        if ($matches['minute']) {
+            $i = $matches['minute'].'M';
+        } else {
+            $i = null;
+        }
+        if ($matches['second']) {
+            $s = $matches['second'].'S';
+        } else {
+            $s = null;
+        }
+
+        if ($h !== null || $i !== null || $s !== null) {
+            $t = 'T'.$h.$i.$s;
+        }
+        $di =  new \DateInterval("P$p$t");
+
+        return $di;
+    }
+}
