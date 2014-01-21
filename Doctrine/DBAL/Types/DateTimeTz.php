b924a9045ab72eadc345776146cfc9827da3ac61
flecomte <flecomte@welcome-media.fr>
2014-01-21 12:32:31 +0100

    Fix DateTimeTz on null value

--- Doctrine/DBAL/Types/DateTimeTz.php
+++ Doctrine/DBAL/Types/DateTimeTz.php
@@ -7,9 +7,24 @@ use Doctrine\DBAL\Types\ConversionException;
 
 class DateTimeTz extends DateTimeTzType
 {
+	protected static $format = 'Y-m-d H:i:s.uP';
+
     public function convertToDatabaseValue ($value, AbstractPlatform $platform)
     {
-        return ($value !== null) ? $value->format('Y-m-d H:i:s.uP') : null;
+    	if ($value === null) {
+    		return null;
+    	} elseif ($value instanceof \DateTime) {
+    		return $value->format(self::$format);
+    	} elseif (is_string($value)) {
+    		try {
+        		$dt = new \DateTime($value);
+        		return $dt->format(self::$format);
+    		} catch (\Exception $e) {
+    			throw new \Exception('Date "'.$value.'" is not a valid date');
+    		}
+    	}
+
+    	throw new \Exception('Date "'.$value.'" is not a valid date');
     }
 
     public function convertToPHPValue ($value, AbstractPlatform $platform)
@@ -20,9 +35,9 @@ class DateTimeTz extends DateTimeTzType
         try {
             return parent::convertToPHPValue($value, $platform);
         } catch (ConversionException $e) {
-            $val = \DateTime::createFromFormat('Y-m-d H:i:s.uP', $value);
+            $val = \DateTime::createFromFormat(self::$format, $value);
             if (! $val) {
-                throw ConversionException::conversionFailedFormat($value, $this->getName(), 'Y-m-d H:i:s.uP');
+                throw ConversionException::conversionFailedFormat($value, $this->getName(), self::$format);
             }
 
             return $val;
