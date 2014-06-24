41eb25e8c9ebbf6d780f72a47a07a61eea30b5c5
flecomte <flecomte@welcome-media.fr>
2014-06-24 18:42:02 +0200

    [Insight] Code should not be duplicated #1

---
+++ Doctrine/DBAL/Types/DateTimeAbstract.php
@@ -0,0 +1,43 @@
+<?php
+namespace FLE\Bundle\PostgresqlTypeBundle\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+
+trait DateTimeTrait
+{
+    public function convertToDatabaseValue ($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        } elseif ($value instanceof \DateTime) {
+            return $value->format(self::$format);
+        } elseif (is_string($value)) {
+            try {
+                $dt = new \DateTime($value);
+                return $dt->format(self::$format);
+            } catch (\Exception $e) {
+                throw new \Exception('Date "'.$value.'" is not a valid date');
+            }
+        }
+
+        throw new \Exception('Date "'.$value.'" is not a valid date');
+    }
+
+    public function convertToPHPValue ($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        }
+        try {
+            return parent::convertToPHPValue($value, $platform);
+        } catch (ConversionException $e) {
+            $val = \DateTime::createFromFormat(self::$format, $value);
+            if (! $val) {
+                throw ConversionException::conversionFailedFormat($value, $this->getName(), self::$format);
+            }
+
+            return $val;
+        }
+    }
+}
